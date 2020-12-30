70ac3f3211e508dc72879bf08002379e5f9244ea
AshishAcharya083 <imashish083@gmail.com>
2020-12-30 21:56:21 +0545

    checkAnswer is not working

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import array from './questionBank';
 import './style.css';
 
 function App(){
-
-    return <Question question={array[0].question} answers={array[0].answers} correctAnswers = {array[0].correct_answers}/>
+    return <Question array={array}/>
+   
 
 }
 
--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,77 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import array from "../questionBank";
 
-export default class Question extends React.Component {
-  constructor(props) {
-    super(props);
-
-    function findAnswer() {
-      const arrayOfCorrectAnswer = Object.values(props.correctAnswers).map(
-        (value, index) => {
-          if (value === "true") {
-            return index;
-          } else {
-            return null;
-          }
-        }
-      );
-      const filteredArray = arrayOfCorrectAnswer.filter((num) => num != null);
-
-      let correctAnswer = filteredArray[0];
-
-      return correctAnswer;
+function findCorrectAnswer(num) {
+  const arrayOfCorrectAnswer = Object.values(array[num].correct_answers).map(
+    (value, index) => {
+      if (value === "true") {
+        return index;
+      } else {
+        return null;
+      }
     }
-
-    console.log(findAnswer());
-
-    // console.log( props.correctAnswers);
-    // function findCorrectAnswer(props) {
-
-    //   console.log(props.correctAnswers);
-    //   // Object.entries(props.correctAnswers).map(item => {
-    //   //   console.log(item);
-    //   //   return item;
-    //   // }
-    //   // )
-    // }
-    // findCorrectAnswer();
-    // console.log("the correct answer is at index " + findCorrectAnswer());
-    this.state = {
-      questionNumber: 0,
-      question: props.question,
-      firstAnswer: props.answers.answer_a,
-      secondAnswer: props.answers.answer_a,
-      thirdAnswer: props.answers.answer_c,
-      fourthAnswer: props.answers.answer_d,
-      fifthAnswer: props.answers.answer_e,
-    };
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="dotted"></div>
-        <h2>{this.state.question}</h2>
-        <ul className="options">
-          <li>
-            <input type="radio" id="0" name="gender" value="male" />
-            <label for="0">{this.state.firstAnswer}</label>
-          </li>
-          <li>
-            <input type="radio" id="1" name="gender" value="male" />
-            <label for="1">{this.state.secondAnswer}</label>
-          </li>
-          <li>
-            <input type="radio" id="2" name="gender" value="male" />
-            <label for="2">{this.state.thirdAnswer}</label>
-          </li>
-          <li>
-            <input type="radio" id="3" name="gender" value="male" />
-            <label for="3">{this.state.fourthAnswer}</label>
-          </li>
-        </ul>
-        <div className="dotted"></div>
-        <button className="next-button">Next</button>
-      </div>
-    );
-  }
+  );
+  console.log('the correct answer array is ' +  arrayOfCorrectAnswer + 'for index ' + num)
+  const filteredArray = arrayOfCorrectAnswer.filter((num) => num != null);
+  let correctAnswer = filteredArray[0];
+  console.log("The correct answer is " + correctAnswer + ' for index ' + num);
+  return correctAnswer;
 }
+
+function Question(props) {
+  const [index, setIndex] = useState(0);
+  const [answer, setAnswer] = useState(0);
+  const [score, setScore] = useState(0);
+
+  function checkAnswer(){
+    console.log('answer checked for ' + index);
+    if(answer === findSelectedRadioButton()){
+        console.log('CORRECT ANSWER!!!');
+    }
+  }
+
+  function clicked() {
+    console.log('button clicked');
+    setAnswer(findCorrectAnswer(index));
+    
+    checkAnswer();
+    
+    console.log('index is ' + index);
+    console.log('answer is ' + answer);
+    setIndex(index + 1);
+  }
+  function findSelectedRadioButton(){
+    let selections = document.getElementsByName('selection');
+    let selected;
+    for(let  i = 0 ; i< 4; i++){
+        if(selections[i].checked){
+          selected = selections[i].value;
+        }
+    } 
+    console.log("The selected RADIO BUTTON is " + selected);
+    return selected;
+  }
+
+
+  let tempQues = array[index].answers;
+  return (
+    <div className="container">
+      <div className = 'indicators'>
+        <h3 className="question-number">
+          question number <span className="question-integer"> {index + 1}</span>
+        </h3>
+        <h3 className="point">
+          My score <span className="question-integer">{score}</span>
+        </h3>
+      </div>
+      <div className="dotted"></div>
+      <h2>{array[index].question}</h2>
+      <ul className="options">
+        <li>
+          <input type="radio" id="0" name="selection" value="0" />
+          <label for="0">{tempQues.answer_a}</label>
+        </li>
+        <li>
+          <input type="radio" id="1" name="selection" value="1" />
+          <label for="1">{tempQues.answer_b}</label>
+        </li>
+        <li>
+          <input type="radio" id="2" name="selection" value="2" />
+          <label for="2">{tempQues.answer_c}</label>
+        </li>
+        <li>
+          <input type="radio" id="3" name="selection" value="3" />
+          <label for="3">{tempQues.answer_d}</label>
+        </li>
+      </ul>
+      <div className="dotted"></div>
+      <div></div>
+      <button onClick={clicked} className="next-button">
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Question;
