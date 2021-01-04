fd4f4eeb9d5abb7ae02bf8b5e0546e2658d6d758
AshishAcharya083 <imashish083@gmail.com>
2021-01-04 14:00:34 +0545

    cleaned the whole code and started again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Question from './components/question';
-import array from './questionBank';
+
 import './style.css';
 
 function App(){
-    return <Question array={array}/>
+    return <div>Starting</div>
    
 
 }
